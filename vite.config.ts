c836d3b1910e3a022492d39247f70c8c6eb7b7bc
Hermès Hounkonnou <agapethounk@gmail.com>
2026-03-09 18:05:03 +0100

    Add allowedHosts configuration to server settings

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,9 @@ export default defineConfig((config) => {
      preview: {
       allowedHosts: true,
     },
+    server: {
+      allowedHosts: true,
+    },
     define: {
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
     },
